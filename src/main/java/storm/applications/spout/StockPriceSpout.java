a544381915e8ad2129bd8abe953fe2451ac60bde
Maycon Bordin <mayconbordin@gmail.com>
2014-07-11 09:10:16 -0300

    StockPriceSpout now extends the AbstractSpout, with dynamic field declaration, configuration of quote fetcher, symbols, etc. Possibility to fetch multiple quotes, using a queue to store quotes, fetch new quotes when empty queue.

--- src/main/java/storm/applications/spout/StockPriceSpout.java
+++ src/main/java/storm/applications/spout/StockPriceSpout.java
@@ -1,79 +1,99 @@
 package storm.applications.spout;
 
-import backtype.storm.spout.SpoutOutputCollector;
-import backtype.storm.task.TopologyContext;
-import backtype.storm.topology.OutputFieldsDeclarer;
-import backtype.storm.topology.base.BaseRichSpout;
-import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Values;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
+import java.util.concurrent.LinkedBlockingQueue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import storm.applications.constants.BargainIndexConstants.Conf;
 import storm.applications.model.finance.Quote;
 import storm.applications.model.finance.QuoteFetcher;
+import storm.applications.util.ClassLoaderUtils;
+import storm.applications.util.ConfigUtility;
 
 /**
  * Change name to TAQ (Trade and Quotes)
  * @author mayconbordin <mayconbordin@gmail.com>
  */
-public class StockPriceSpout extends BaseRichSpout {
+public class StockPriceSpout extends AbstractSpout {
     private static Logger LOG = LoggerFactory.getLogger(StockPriceSpout.class);
 
-    private SpoutOutputCollector outputCollector;
     private QuoteFetcher fetcher;
-    private String symbol;
-    private int days = 30;
+    private LinkedBlockingQueue<Quote> queue;
     
-    /**
-     * Interval between quotes in seconds.
-     */
-    private int interval = 60;
-    private List<Quote> quotes;
-    private int nextQuote = 0;
-
-    public StockPriceSpout(QuoteFetcher fetcher, String symbol) {
-        this.fetcher = fetcher;
-        this.symbol = symbol;
-    }
+    private String[] symbols;
+    private int days;
+    private int interval;
     
-    public StockPriceSpout(QuoteFetcher fetcher, String symbol, int days) {
-        this.fetcher = fetcher;
-        this.symbol = symbol;
-        this.days = days;
-    }
-    
-    public StockPriceSpout(QuoteFetcher fetcher, String symbol, int days, int interval) {
-        this.fetcher = fetcher;
-        this.symbol = symbol;
-        this.days = days;
-        this.interval = interval;
-    }
+    private String fetcherKey  = Conf.SPOUT_FETCHER;
+    private String symbolsKey  = Conf.SPOUT_SYMBOLS;
+    private String daysKey     = Conf.SPOUT_DAYS;
+    private String intervalKey = Conf.SPOUT_INTERVAL;
 
     @Override
-    public void declareOutputFields(OutputFieldsDeclarer declarer) {
-        declarer.declare(new Fields("stock", "quote"));
+    public void initialize() {
+        days     = ConfigUtility.getInt(config, daysKey);
+        interval = ConfigUtility.getInt(config, intervalKey);
+        
+        String symbolsStr   = ConfigUtility.getString(config, symbolsKey);
+        symbols = symbolsStr.split(",");
+        
+        String fetcherClass = ConfigUtility.getString(config, fetcherKey);
+        fetcher = (QuoteFetcher) ClassLoaderUtils.newInstance(fetcherClass, "fetcher", LOG);
+        
+        queue = new LinkedBlockingQueue<>();
+        
+        fetchPrices();
     }
-
-    @Override
-    public void open(Map conf, TopologyContext context, SpoutOutputCollector collector) {
-        this.outputCollector = collector;
+    
+    private void fetchPrices() {
+        List<Quote> quotes = new ArrayList<>();
         
-        // need to diferentiate between quotes and trades
-        
-        try {
-            String quoteStr = fetcher.fetchQuotes(symbol, days, interval);
-            quotes = fetcher.parseQuotes(quoteStr, interval);
-        } catch (Exception ex) {
-            LOG.error("Unable to fetch quotes", ex);
+        for (String symbol : symbols) {
+            try {
+                String quoteStr = fetcher.fetchQuotes(symbol, days, interval);
+                quotes.addAll(fetcher.parseQuotes(symbol, quoteStr, interval));
+            } catch (Exception ex) {
+                LOG.error("Unable to fetch quotes", ex);
+            }
         }
+        
+        Collections.sort(quotes, new Comparator<Quote>() {
+            @Override
+            public int compare(Quote a, Quote b) {
+                return a.getOpenDate().compareTo(b.getOpenDate());
+            }
+        });
+        
+        queue.addAll(quotes);
     }
 
     @Override
     public void nextTuple() {
-        if (nextQuote < quotes.size()) {
-            outputCollector.emit(new Values(symbol, quotes.get(nextQuote++)));
+        Quote quote = queue.poll();
+        
+        if (quote != null) {
+            collector.emit(new Values(quote.getSymbol(), quote));
         }
+    }
+
+    public void setFetcherKey(String fetcherKey) {
+        this.fetcherKey = fetcherKey;
+    }
+
+    public void setSymbolsKey(String symbolsKey) {
+        this.symbolsKey = symbolsKey;
+    }
+
+    public void setDaysKey(String daysKey) {
+        this.daysKey = daysKey;
+    }
+
+    public void setIntervalKey(String intervalKey) {
+        this.intervalKey = intervalKey;
     }
     
 }
