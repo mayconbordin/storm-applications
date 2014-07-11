f1dcc8f0c97347349472db1c0e7d4fd55090805a
Maycon Bordin <mayconbordin@gmail.com>
2014-07-11 17:41:04 -0300

    Simplified the abstract topology

--- src/main/java/storm/applications/topology/AbstractTopology.java
+++ src/main/java/storm/applications/topology/AbstractTopology.java
@@ -3,6 +3,7 @@ package storm.applications.topology;
 import backtype.storm.Config;
 import backtype.storm.generated.StormTopology;
 import backtype.storm.topology.TopologyBuilder;
+import org.slf4j.Logger;
 
 public abstract class AbstractTopology {
     protected TopologyBuilder builder;
@@ -12,19 +13,14 @@ public abstract class AbstractTopology {
     public AbstractTopology(String topologyName, Config config) {
         this.topologyName = topologyName;
         this.config = config;
-        
-        initialize();
-    }
-    
-    private void initialize() {
         this.builder = new TopologyBuilder();
-        prepare();
     }
 
     public String getTopologyName() {
         return topologyName;
     }
 
-    public abstract void prepare();
+    public abstract void initialize();
     public abstract StormTopology buildTopology();
+    public abstract Logger getLogger();
 }
