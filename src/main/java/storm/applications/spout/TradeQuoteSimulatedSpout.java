cd1989612f3e80881fc4e2169dac22e32a3e24d1
Maycon Bordin <mayconbordin@gmail.com>
2014-07-11 09:15:44 -0300

    Removed unused imports.

--- src/main/java/storm/applications/spout/TradeQuoteSimulatedSpout.java
+++ src/main/java/storm/applications/spout/TradeQuoteSimulatedSpout.java
@@ -1,18 +1,10 @@
 package storm.applications.spout;
 
-import backtype.storm.spout.SpoutOutputCollector;
-import backtype.storm.task.TopologyContext;
-import backtype.storm.topology.OutputFieldsDeclarer;
-import backtype.storm.topology.base.BaseRichSpout;
-import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Values;
-import java.util.List;
-import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import static storm.applications.constants.BargainIndexConstants.*;
 import storm.applications.model.finance.Quote;
-import storm.applications.model.finance.QuoteFetcher;
 import storm.applications.util.RandomUtil;
 
 /**
