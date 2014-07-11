d7caf70798f1f1a423b3fcbf0e9187ef4e53eb40
Maycon Bordin <mayconbordin@gmail.com>
2014-07-11 09:14:53 -0300

    TradeQuoteSimulatedSpout has a very poor "simulation" of quotes. The spout now extends the StockPriceSpout since only the emission of tuples differs.

--- src/main/java/storm/applications/spout/TradeQuoteSimulatedSpout.java
+++ src/main/java/storm/applications/spout/TradeQuoteSimulatedSpout.java
@@ -19,67 +19,15 @@ import storm.applications.util.RandomUtil;
  * Fetches 
  * @author mayconbordin <mayconbordin@gmail.com>
  */
-public class TradeQuoteSimulatedSpout extends BaseRichSpout {
+public class TradeQuoteSimulatedSpout extends StockPriceSpout {
     private static Logger LOG = LoggerFactory.getLogger(TradeQuoteSimulatedSpout.class);
-
-    private SpoutOutputCollector outputCollector;
-    private QuoteFetcher fetcher;
-    private String symbol;
-    private int days = 30;
-    
-    /**
-     * Interval between quotes in seconds.
-     */
-    private int interval = 60;
-    private List<Quote> quotes;
-    private int nextQuote = 0;
-
-    public TradeQuoteSimulatedSpout(QuoteFetcher fetcher, String symbol) {
-        this.fetcher = fetcher;
-        this.symbol = symbol;
-    }
-    
-    public TradeQuoteSimulatedSpout(QuoteFetcher fetcher, String symbol, int days) {
-        this.fetcher = fetcher;
-        this.symbol = symbol;
-        this.days = days;
-    }
-    
-    public TradeQuoteSimulatedSpout(QuoteFetcher fetcher, String symbol, int days, int interval) {
-        this.fetcher = fetcher;
-        this.symbol = symbol;
-        this.days = days;
-        this.interval = interval;
-    }
-
-    @Override
-    public void declareOutputFields(OutputFieldsDeclarer declarer) {
-        //declarer.declare(new Fields("stock", "quote"));
-        declarer.declareStream(TRADE_STREAM, new Fields(STOCK_FIELD, PRICE_FIELD, 
-                VOLUME_FIELD, DATE_FIELD, INTERVAL_FIELD));
-        declarer.declareStream(QUOTE_STREAM, new Fields(STOCK_FIELD, PRICE_FIELD, 
-                VOLUME_FIELD, DATE_FIELD));
-    }
-
-    @Override
-    public void open(Map conf, TopologyContext context, SpoutOutputCollector collector) {
-        this.outputCollector = collector;
-        
-        // need to diferentiate between quotes and trades
-        
-        try {
-            String quoteStr = fetcher.fetchQuotes(symbol, days, interval);
-            quotes = fetcher.parseQuotes(quoteStr, interval);
-        } catch (Exception ex) {
-            LOG.error("Unable to fetch quotes", ex);
-        }
-    }
 
     @Override
     public void nextTuple() {
-        if (nextQuote < quotes.size()) {
-            Quote quote = quotes.get(nextQuote++);
-            outputCollector.emit(TRADE_STREAM, new Values(symbol, quote.getAverage(), quote.getVolume(), quote.getOpenDate(), quote.getInterval()));
+        Quote quote = queue.poll();
+        
+        if (quote != null) {
+            collector.emit(Stream.TRADES, new Values(quote.getSymbol(), quote.getAverage(), quote.getVolume(), quote.getOpenDate(), quote.getInterval()));
             
             // here we simulate an askPrice that range between 90% and 110% of the
             // average current price for the same symbol
@@ -88,7 +36,7 @@ public class TradeQuoteSimulatedSpout extends BaseRichSpout {
             // the size offered ranges from 10% to 80% of the volume
             int askSize = RandomUtil.randInt((int)(quote.getVolume()*0.1), (int)(quote.getVolume()*0.8));
             
-            outputCollector.emit(QUOTE_STREAM, new Values(symbol, askPrice, askSize, quote.getOpenDate().plusMinutes(2)));
+            collector.emit(Stream.QUOTES, new Values(quote.getSymbol(), askPrice, askSize, quote.getOpenDate().plusMinutes(2)));
         }
     }
     
