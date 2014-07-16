25958624aaf5c1e4b223716c1651e54d5efed117
Maycon Bordin <mayconbordin@gmail.com>
2014-07-16 09:27:02 -0300

    Refactored SentimentAnalysis application

--- src/main/java/storm/applications/constants/SentimentAnalysisConstants.java
+++ src/main/java/storm/applications/constants/SentimentAnalysisConstants.java
@@ -1,6 +1,26 @@
 package storm.applications.constants;
 
-public class SentimentAnalysisConstants {
-    public static final String TWITTER_SPOUT = "twitterSpout";
-    public static final String SENTIMENT_BOLT = "sentimentBolt";
+public interface SentimentAnalysisConstants extends BaseConstants {
+    String PREFIX = "sa";
+    
+    interface Conf extends BaseConf {
+        String CLASSIFIER_THREADS       = "sa.classifier.threads";
+        String CLASSIFIER_TYPE          = "sa.classifier.type";
+        String LINGPIPE_CLASSIFIER_PATH = "sa.classifier.lingpipe.path";
+        String BASIC_CLASSIFIER_PATH    = "sa.classifier.basic.path";
+    }
+    
+    interface Component extends BaseComponent {
+        String CLASSIFIER = "classifierBolt";
+    }
+    
+    interface Field {
+        String TWEET = "tweet";
+        //"id", "text", "timestamp", "sentiment", "score"
+        String ID = "id";
+        String TEXT = "text";
+        String TIMESTAMP = "timestamp";
+        String SENTIMENT = "sentiment";
+        String SCORE = "score";
+    }
 }
--- src/main/java/storm/applications/topology/SentimentAnalysisTopology.java
+++ src/main/java/storm/applications/topology/SentimentAnalysisTopology.java
@@ -2,13 +2,11 @@ package storm.applications.topology;
 
 import backtype.storm.Config;
 import backtype.storm.generated.StormTopology;
-import backtype.storm.topology.TopologyBuilder;
 import backtype.storm.tuple.Fields;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import storm.applications.bolt.CalculateSentimentBolt;
-import storm.applications.bolt.LocateStateBolt;
-import storm.applications.spout.TwitterStreamSpout;
-import storm.applications.topology.AbstractTopology;
-import twitter4j.FilterQuery;
+import static storm.applications.constants.SentimentAnalysisConstants.*;
 
 /**
  * Orchestrates the elements and forms a Topology to find the most happiest state
@@ -17,45 +15,43 @@ import twitter4j.FilterQuery;
  * 
  * @author Saurabh Dubey <147am@gmail.com>
  */
-public class SentimentAnalysisTopology extends AbstractTopology {
-
+public class SentimentAnalysisTopology extends BasicTopology {
+    private static final Logger LOG = LoggerFactory.getLogger(SentimentAnalysisTopology.class);
+    
+    private int classifierThreads;
+    
     public SentimentAnalysisTopology(String topologyName, Config config) {
         super(topologyName, config);
     }
 
-    public void prepare() {
-        
+    @Override
+    public void initialize() {
+        classifierThreads = config.getInt(Conf.CLASSIFIER_THREADS, 1);
     }
     
     @Override
     public StormTopology buildTopology() {
-        builder = new TopologyBuilder();
+        spout.setFields(new Fields(Field.TWEET));
         
+        builder.setSpout(Component.SPOUT, spout, spoutThreads);
+
+        builder.setBolt(Component.CLASSIFIER, new CalculateSentimentBolt(), classifierThreads)
+               .shuffleGrouping(Component.SPOUT);
         
-        // TODO: http://altf1be.wordpress.com/2013/06/14/designing-a-solution-using-real-time-stock-quotes-analytics-with-storm-twitter-yahoo-finance-bigdata-sentimentanalysis-ghent-belgium/
-        // tweets from stock quotes
-        
-        // implement the sentiment analysis as an engine
-        // so that we can have multiple engines
-        
-        //Bounding Box for United States.
-        //For more info on how to get these coordinates, check:
-        //http://www.quora.com/Geography/What-is-the-longitude-and-latitude-of-a-bounding-box-around-the-continental-United-States
-        double[][] boundingBox = {{-124.848974, 24.396308}, {-66.885444, 49.384358}};
-        
-        FilterQuery filterQuery = new FilterQuery();
-        filterQuery.locations(boundingBox);
-        
-        builder.setSpout("twitterspout", new TwitterStreamSpout(filterQuery));
-        
-        builder.setBolt("statelocatorbolt", new LocateStateBolt())
-               .shuffleGrouping("twitterspout");
-        
-        //Create Bolt with the frequency of logging [in seconds].
-        builder.setBolt("sentimentcalculatorbolt", new CalculateSentimentBolt(30))
-               .fieldsGrouping("statelocatorbolt", new Fields("state"));
+        builder.setBolt(Component.SINK, sink, sinkThreads)
+               .shuffleGrouping(Component.CLASSIFIER);
         
         return builder.createTopology();
+    }
+
+    @Override
+    public Logger getLogger() {
+        return LOG;
+    }
+
+    @Override
+    public String getConfigPrefix() {
+        return PREFIX;
     }
     
 }
