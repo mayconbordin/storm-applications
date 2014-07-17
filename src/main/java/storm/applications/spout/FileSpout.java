9e44c11a52d8d7ba092708d4ae53a276bd571749
Maycon Bordin <mayconbordin@gmail.com>
2014-07-17 08:36:18 -0300

    Included more information about the files being read by the spout

--- src/main/java/storm/applications/spout/FileSpout.java
+++ src/main/java/storm/applications/spout/FileSpout.java
@@ -6,11 +6,13 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import storm.applications.constants.BaseConstants.BaseConf;
 import storm.applications.spout.parser.Parser;
 import storm.applications.util.ClassLoaderUtils;
+import storm.applications.util.FileUtils;
 import storm.applications.util.StreamValues;
 
 public class FileSpout extends AbstractSpout {
@@ -20,6 +22,7 @@ public class FileSpout extends AbstractSpout {
     protected File[] files;
     protected Scanner scanner;
     protected int curFileIndex = 0;
+    protected boolean finished = false;
 
     @Override
     public void initialize() {
@@ -32,7 +35,19 @@ public class FileSpout extends AbstractSpout {
     }
     
     protected void buildIndex() {
-        File dir = new File(config.getString(getConfigKey(BaseConf.SPOUT_PATH)));
+        String path = config.getString(getConfigKey(BaseConf.SPOUT_PATH));
+        if (StringUtils.isBlank(path)) {
+            LOG.error("The source path has not been set");
+            throw new RuntimeException("The source path has to beeen set");
+        }
+        
+        LOG.info("Source path: {}", path);
+        
+        File dir = new File(path);
+        if (!dir.exists()) {
+            LOG.error("The source path {} does not exists", path);
+            throw new RuntimeException("The source path '" + path + "' does not exists");
+        }
         
         if (dir.isDirectory()) {
             files = dir.listFiles();
@@ -48,6 +63,8 @@ public class FileSpout extends AbstractSpout {
                 return res;
             }
         });
+        
+        LOG.info("Number of files to read: {}", files.length);
     }
     
     @Override
@@ -66,7 +83,10 @@ public class FileSpout extends AbstractSpout {
     }
 
     protected String readFile() {
+        if (finished) return null;
+        
         String record = null;
+        
         if (scanner.hasNextLine()) {
             record = scanner.nextLine();
         } else {
@@ -77,16 +97,22 @@ public class FileSpout extends AbstractSpout {
                 }				 
             } else {
                 LOG.info("No more files to read");
+                finished = true;
             }
         }
+        
         return record;
     }
 
     protected void openNextFile() {
         try {
-            scanner = new Scanner(files[curFileIndex]);
+            File file = files[curFileIndex];
+            scanner = new Scanner(file);
+            LOG.info("Opened file {}, size {}", file.getName(), 
+                    FileUtils.humanReadableByteCount(file.length()));
+            
         } catch (FileNotFoundException e) {
-            LOG.error("File " + files[curFileIndex] + " was not found", e);
+            LOG.error(String.format("File %s not found", files[curFileIndex]), e);
             throw new IllegalStateException("file not found");
         }
     }
