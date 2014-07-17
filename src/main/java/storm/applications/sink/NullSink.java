cf5b11bf24eece841b100a419eb303bbb699db7f
Maycon Bordin <mayconbordin@gmail.com>
2014-07-17 09:16:28 -0300

    Added a null sink

---
+++ src/main/java/storm/applications/sink/NullSink.java
@@ -0,0 +1,24 @@
+package storm.applications.sink;
+
+import backtype.storm.tuple.Tuple;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author mayconbordin
+ */
+public class NullSink extends BaseSink {
+    private static final Logger LOG = LoggerFactory.getLogger(NullSink.class);
+    
+    @Override
+    public void execute(Tuple input) {
+        // do nothing
+    }
+
+    @Override
+    protected Logger getLogger() {
+        return LOG;
+    }
+    
+}
