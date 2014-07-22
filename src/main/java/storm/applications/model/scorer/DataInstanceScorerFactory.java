dd2df189a2d807549f5485b1b73d0af3cd14d13f
Maycon Bordin <mayconbordin@gmail.com>
2014-07-22 19:52:29 -0300

    Using switch-case in the scorer factory and logging in case of invalid scorer

--- src/main/java/storm/applications/model/scorer/DataInstanceScorerFactory.java
+++ src/main/java/storm/applications/model/scorer/DataInstanceScorerFactory.java
@@ -1,18 +1,19 @@
 package storm.applications.model.scorer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class DataInstanceScorerFactory {
+    private static final Logger LOG = LoggerFactory.getLogger(DataInstanceScorerFactory.class);
+    
     public static DataInstanceScorer getDataInstanceScorer(String dataTypeName) {
-        if (dataTypeName.equals("machineMetadata")) {
-            return new MachineDataInstanceScorer();
+        switch (dataTypeName) {
+            case "machineMetadata":
+                return new MachineDataInstanceScorer();
+                
+            default:
+                LOG.error("{} is not a valid data type for the Scorer", dataTypeName);
+                return null;
         }
-        /*else if(dataTypeName.equals("twitterData")) {
-                return new TwitterDataInstanceScorer();
-        }*/
-        try {
-            throw new Exception("No matched data type scorer for " + dataTypeName);
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
-        return null;
     }
 }
