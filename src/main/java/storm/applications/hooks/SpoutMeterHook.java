3ffb0b4c5d21f9b9399f7cc8ea76f58365da2e39
Maycon Bordin <mayconbordin@gmail.com>
2014-08-26 17:45:50 -0300

    Getting complete latency from spout ack info

--- src/main/java/storm/applications/hooks/SpoutMeterHook.java
+++ src/main/java/storm/applications/hooks/SpoutMeterHook.java
@@ -2,10 +2,13 @@ package storm.applications.hooks;
 
 import backtype.storm.hooks.BaseTaskHook;
 import backtype.storm.hooks.info.EmitInfo;
+import backtype.storm.hooks.info.SpoutAckInfo;
 import backtype.storm.task.TopologyContext;
 import com.codahale.metrics.Meter;
 import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Timer;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import storm.applications.metrics.MetricsFactory;
 import storm.applications.util.Configuration;
 
@@ -16,6 +19,7 @@ import storm.applications.util.Configuration;
 public class SpoutMeterHook extends BaseTaskHook {
     private Configuration config;
     private Meter emittedTuples;
+    private Timer completeLatency;
     
     @Override
     public void prepare(Map conf, TopologyContext context) {
@@ -26,11 +30,19 @@ public class SpoutMeterHook extends BaseTaskHook {
         String componentId = context.getThisComponentId();
         String taskId      = String.valueOf(context.getThisTaskId());
         
-        emittedTuples  = registry.meter(MetricRegistry.name("emitted", componentId, taskId));
+        emittedTuples   = registry.meter(MetricRegistry.name("emitted", componentId, taskId));
+        completeLatency = registry.timer(MetricRegistry.name("complete-latency", componentId, taskId));
     }
 
     @Override
     public void emit(EmitInfo info) {
         emittedTuples.mark();
+    }
+
+    @Override
+    public void spoutAck(SpoutAckInfo info) {
+        if (info.completeLatencyMs != null) {
+            completeLatency.update(info.completeLatencyMs, TimeUnit.MILLISECONDS);
+        }
     }
 }
