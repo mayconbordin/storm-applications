b9523316be3076f8824e96aa11ea4c7c02076d98
Maycon Bordin <mayconbordin@gmail.com>
2014-08-26 17:11:54 -0300

    Meter hook for bolts tracks emitted and received tuples, plus processing latency. Sink meter tracks received tuples and overall latency.

--- src/main/java/storm/applications/hooks/SinkMeterHook.java
+++ src/main/java/storm/applications/hooks/SinkMeterHook.java
@@ -2,13 +2,13 @@ package storm.applications.hooks;
 
 import backtype.storm.hooks.BaseTaskHook;
 import backtype.storm.hooks.info.BoltExecuteInfo;
-import backtype.storm.hooks.info.EmitInfo;
 import backtype.storm.task.TopologyContext;
 import com.codahale.metrics.Meter;
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.Timer;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import static storm.applications.constants.BaseConstants.BaseField.*;
 import storm.applications.metrics.MetricsFactory;
 import storm.applications.util.Configuration;
 
@@ -16,12 +16,12 @@ import storm.applications.util.Configuration;
  *
  * @author mayconbordin
  */
-public class BoltMeterHook extends BaseTaskHook {
+public class SinkMeterHook extends BaseTaskHook {
     private Configuration config;
     
-    private Meter emittedTuples;
     private Meter receivedTuples;
     private Timer processLatency;
+    private Timer overallLatency;
     
     @Override
     public void prepare(Map conf, TopologyContext context) {
@@ -32,22 +32,23 @@ public class BoltMeterHook extends BaseTaskHook {
         String componentId = context.getThisComponentId();
         String taskId      = String.valueOf(context.getThisTaskId());
 
-        emittedTuples  = registry.meter(MetricRegistry.name("emitted", componentId, taskId));
         receivedTuples = registry.meter(MetricRegistry.name("received", componentId, taskId));
         processLatency = registry.timer(MetricRegistry.name("process_latency", componentId, taskId));
+        overallLatency = registry.timer(MetricRegistry.name("overall_latency", componentId, taskId));
     }
 
     @Override
     public void boltExecute(BoltExecuteInfo info) {
+        long now = System.currentTimeMillis();
         receivedTuples.mark();
         
+        if (info.tuple != null && info.tuple.contains(CREATED_AT)) {
+            long createdAt = info.tuple.getLongByField(CREATED_AT);
+            overallLatency.update(now - createdAt, TimeUnit.MILLISECONDS);
+        }
+
         if (info.executeLatencyMs != null) {
             processLatency.update(info.executeLatencyMs, TimeUnit.MILLISECONDS);
         }
-    }
-
-    @Override
-    public void emit(EmitInfo info) {
-        emittedTuples.mark();
     }
 }
